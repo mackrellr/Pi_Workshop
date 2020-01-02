106ea3dc931b2e9d5bdfb2b3ae17634a19579103
Rich Mackrell <58283498+mackrellr@users.noreply.github.com>
2020-01-02 15:46:54 -0500

    Add files via upload

---
+++ c2_camera_1_pic.py
@@ -0,0 +1,11 @@
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+# camera.rotation = 180
+
+# camera.start_preview(alpha=200)
+camera.start_preview()
+sleep(5)
+# camera.capture('home/pi/Desktop/image.jpg')
+camera.stop_preview()
